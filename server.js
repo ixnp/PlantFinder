7c03388404f3e3ad2f7271e20aac36e81b6b3f0e
ixnp <enprocopios@gmail.com>
2017-05-13 14:29:11 -0700

    added postgress database

--- server.js
+++ server.js
@@ -5,14 +5,26 @@ const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const requestProxy = require('express-request-proxy');
-const PORT = process.env.PORT || 3000;
+const $ = require('jquery');
 const app = express();
+// const request = require('superagent');
+const PORT = process.env.PORT || 3000;
+// Required hy heroku//
+
 const conString = 'postgress://postgres:1234@5432:plant_finder';
-// change once live on horku//
+// change once live on heroku//
 const client = new pg.Client(consString);
 client.connect();
 client.on('error', err => console.log(err));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static('./public'));
+app.use(express.static(_dirname + './public'));
+// app.get('/github', function(req,res){
+//   superagent.get('')
+// });
+//may not need this for get hub token//
+
+app.listen(PORT, function(){
+  console.log('server up on port:' +PORT);
+});
