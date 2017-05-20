f72acb002fa599d4225739313e67b07bde716a6f
ixnp <enprocopios@gmail.com>
2017-05-19 22:45:56 -0700

    fixed static routing

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const express = require('express');
 const app = express ();
 
 app.get('/', function (req, res){
-  res.sendFile(__dirname + '/index.html');
-
+res.sendFile(__dirname + '/index.html');
 });
 app.use(express.static(__dirname + '/public'));
 
